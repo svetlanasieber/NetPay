2034e4f5d3441153e976fbd6494be29c4d6a76e9
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-05-09 03:23:03 +0200

    Update ValidationConstants.cs

--- Common/ValidationConstants.cs
+++ Common/ValidationConstants.cs
@@ -2,7 +2,7 @@ namespace NetPay.Common
 {
     public static class ValidationConstants
     {
-        // Household
+
         public const int HouseholdContactPersonMinLength = 5;
         public const int HouseholdContactPersonMaxLength = 50;
 
@@ -12,18 +12,18 @@ namespace NetPay.Common
         public const int PhoneNumberLength = 15;
         public const string PhoneNumberPattern = @"^\+\d{3}/\d{3}-\d{6}$";
 
-        // Expense
+      
         public const int ExpenceNameMinLength = 5;
         public const int ExpenceNameMaxLength = 50;
 
         public const string MinAmount = "0.01";
         public const string MaxAmount = "100000";
 
-        // Service
+    
         public const int ServiceNameMinLength = 5;
         public const int ServiceNameMaxLength = 30;
 
-        // Supplier
+      
         public const int SupplierNameMinLength = 3;
         public const int SupplierNameMaxLength = 60;
     }
